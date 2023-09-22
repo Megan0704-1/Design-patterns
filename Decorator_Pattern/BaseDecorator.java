bd31eda9a26866064a9c4d84c1ea6f91e5cd7383
Kuo, Mei-Chun <94007620+Megan0704-1@users.noreply.github.com>
2023-09-22 22:31:34 +0800

    Delete Decorator_Pattern directory

--- Decorator_Pattern/BaseDecorator.java
+++
@@ -1,9 +0,0 @@
-// Base decorator class //
-public abstract class BaseDecorator extends Beverage {
-    // using Beverage as supertype
-
-    // this is the beverage that each decorator will be wrapping
-    Beverage coffee; //instance variable
-
-    public abstract String getDescription();
-}
